a6dc8307fa2025da7052940c5e1cbee70b9e356b
Anaïs Betts <anais@anaisbetts.org>
2023-10-31 22:39:49 +0100

    Dockerize it

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
   },
+  swcMinify: true,
+  output: "standalone",
 };
 
 module.exports = nextConfig
