97afd6707278d398122f33748ff63f8aa4aa0c26
Anaïs Betts <anais@anaisbetts.org>
2023-10-31 18:26:56 +0100

    dotfile junk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [{ protocol: "https", hostname: "image.tmdb.org" }],
+  },
+};
 
 module.exports = nextConfig
