56e1ff35aeb93d73c566cc18a029ea00341b8efa
Anaïs Betts <anais@anaisbetts.org>
2023-11-01 19:11:26 +0100

    Do stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import Status, { PlayThatVideo } from "./client/client"
+import { status } from "./server/api"
 
-export default function Home() {
-  const sekrit = 'kF4Q0iM8G7wEC6vHiJLqu19nxwt'
+export default async function Home() {
+  try {
+    await status();
+  } catch (e) {
+    return ( <h2>We need a client :P</h2>)
+  }
 
   return (
     <main className="min-h-screen">
-      <Status />
-      <Image src={`https://image.tmdb.org/t/p/w780//${sekrit}.jpg`} alt="" width={300} height={300} />
-
-      <PlayThatVideo />
+      <h2>u did it</h2>
     </main>
   )
 }
