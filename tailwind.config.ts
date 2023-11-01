5510be909b579b298e8c52a7c4956e4a5f03b5d4
Anaïs Betts <anais@anaisbetts.org>
2023-11-01 22:20:26 +0100

    Add neat stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
+
 export default config
