1b8f165d817a71cd8ef20dd17e6da6d523576b9a
Anaïs Betts <anais@anaisbetts.org>
2023-11-03 19:18:07 +0100

    Steal some Tailwind stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss'
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config: Config = {
+  mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +11,57 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      transitionProperty: {
+        'max-height': 'max-height',
+        width: 'width',
       },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: theme('colors.indigo.500'),
+              '&:hover': {
+                color: theme('colors.indigo.400'),
+              },
+            },
+
+            h1: {
+              color: theme('colors.gray.300'),
+            },
+            h2: {
+              color: theme('colors.gray.300'),
+            },
+            h3: {
+              color: theme('colors.gray.300'),
+            },
+            h4: {
+              color: theme('colors.gray.300'),
+            },
+            h5: {
+              color: theme('colors.gray.300'),
+            },
+            h6: {
+              color: theme('colors.gray.300'),
+            },
+
+            strong: {
+              color: theme('colors.gray.400'),
+            },
+
+            code: {
+              color: theme('colors.gray.300'),
+            },
+
+            figcaption: {
+              color: theme('colors.gray.500'),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
