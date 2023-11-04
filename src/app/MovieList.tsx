84f3a69d417e703b08df5addef65700c90dbdd99
Anaïs Betts <anais@anaisbetts.org>
2023-11-04 15:23:36 +0100

    💅

--- src/app/MovieList.tsx
+++ src/app/MovieList.tsx
@@ -1,23 +1,39 @@
+import { cx } from '@/lib/actions/utility'
 import { Movie } from '@/lib/types'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type MovieListProps = {
   movies: Movie[]
 }
 
+export function MovieTile({ movie }: { movie: Movie }) {
+  var c = cx(
+    'flex items-center justify-center',
+    'group relative transform transition-transform hover:scale-110',
+    'rounded-lg object-cover group-hover:opacity-75 transition-opacity',
+    'drop-shadow-xl hover:drop-shadow-2xl'
+  )
+
+  return (
+    <Link href='#' aria-label={movie.title}>
+      <Image
+        className={c}
+        style={{ maxWidth: '150px' }}
+        src={movie.image_url}
+        alt={movie.title}
+        width={200}
+        height={300}
+      />
+    </Link>
+  )
+}
+
 export default function MovieList({ movies }: MovieListProps) {
   return (
-    <div className='grid w-max grid-flow-col grid-rows-2 gap-4 overflow-x-auto px-8'>
+    <div className='grid w-screen grid-flow-col-dense grid-rows-2 gap-4 overflow-x-auto px-8'>
       {movies.map((movie) => (
-        <Image
-          className='flex items-center justify-center'
-          style={{ maxWidth: '150px' }}
-          key={movie.id}
-          src={movie.image_url}
-          alt={movie.title}
-          width={200}
-          height={300}
-        />
+        <MovieTile movie={movie} key={movie.id} />
       ))}
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Source_Sans_3 } from 'next/font/google'
 
 import './globals.css'
+import { cx } from '@/lib/actions/utility'
 
 const sourceSans = Source_Sans_3({ subsets: ['latin'] })
 
@@ -15,10 +16,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const bc = cx(
+    sourceSans.className,
+    'v-screen h-screen prose bg-gradient-to-r from-purple-900 to-blue-900 text-white'
+  )
   return (
     <html lang='en'>
-      <body className={sourceSans.className}>
-        <main className='prose min-h-screen'>{children}</main>
+      <body className={bc}>
+        <main className='py-10'>{children}</main>
       </body>
     </html>
   )
