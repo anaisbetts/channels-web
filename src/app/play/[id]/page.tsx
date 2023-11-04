648e87d23468a54e441960d88803be6c82007622
Anaïs Betts <anais@anaisbetts.org>
2023-11-04 17:24:16 +0100

    Add the worlds crappiest detail page

---
+++ src/app/play/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { Movie } from '@/lib/types'
+import { fetchMovies } from '@/server/api'
+
+export type PlayerPageProps = {
+  params: { id: string }
+}
+
+export default async function PlayerPage({ params }: PlayerPageProps) {
+  const movies = await fetchMovies()
+  const byId = movies.reduce(
+    (acc, x) => {
+      acc[x.id] = x
+      return acc
+    },
+    {} as Record<string, Movie>
+  )
+
+  const theMovie = byId[params.id]
+	<ReactVideo
+  return <h2>The movie is {theMovie.title}</h2>
+}
