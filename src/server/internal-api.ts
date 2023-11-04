3558f2a8fb2b6243f5219ebd1ac6e214d5ad1060
Anaïs Betts <anais@anaisbetts.org>
2023-11-04 20:06:06 +0100

    Store even harder

--- src/server/internal-api.ts
+++ src/server/internal-api.ts
@@ -3,14 +3,27 @@ import 'server-only'
 import { Axios } from 'axios'
 import storage from 'node-persist'
 import { mkdirp } from 'mkdirp'
+import fs from 'fs'
 import path from 'path'
 
 // NB: If we don't do this, we will end up writing to a read-only part
 // inside the Docker image
-const storagePath = path.join(__dirname, '..', '..', '.next', 'storage')
-mkdirp.sync(storagePath)
+const storagePath = path.join('.next')
+const altStoragePath = path.join(__dirname, '..', '..', '.next')
 
-const init = storage.init({ dir: storagePath })
+let sp = altStoragePath
+try {
+  fs.statSync(storagePath)
+  sp = storagePath
+} catch (e) {
+  console.log('Using alternate storage path')
+}
+
+const tgt = path.join(sp, 'storage')
+mkdirp.sync(tgt)
+
+console.log(`Writing config to ${tgt}`)
+const init = storage.init({ dir: tgt })
 
 export async function setClient(baseUrl: string | null) {
   await init
