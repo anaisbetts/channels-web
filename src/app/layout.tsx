bc7eaa6fe342fe96bd3e1d9d6ba4b9c622878c5b
Anaïs Betts <anais@anaisbetts.org>
2023-11-04 18:58:21 +0100

    Add the world's dumbest header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,14 @@ export default function RootLayout({
     sourceSans.className,
     'v-screen h-screen bg-gradient-to-r from-purple-900 to-blue-900 text-white'
   )
+
   return (
     <html lang='en'>
       <body className={bc}>
+        <nav className='bg-gradient-to-r from-purple-700 to-blue-700'>
+          <h2 className='p-4 text-2xl font-bold'>Channels</h2>
+        </nav>
+
         <main>{children}</main>
       </body>
     </html>
