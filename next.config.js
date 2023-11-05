5ebcac132e52ef6c2890c47fbf1dca896f86f41e
Anaïs Betts <anais@anaisbetts.org>
2023-11-05 15:13:12 +0100

    lol

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
       { protocol: 'http', hostname: 'fanc.tmsimg.com' },
       { protocol: 'https', hostname: 'image.tmdb.org' },
       { protocol: 'https', hostname: 'tmsimg.fancybits.co' },
-      { protocol: 'http', hostname: '192.168.4.10', port: '8089' },
     ],
   },
   swcMinify: true,
