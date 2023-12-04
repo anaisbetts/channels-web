21d5dc8d8db0690b99ed42550e4c53f627f86a02
Anaïs Betts <anais@anaisbetts.org>
2023-12-04 16:05:50 +0100

    Fix the data dir stuff

--- src/server/data-dir.ts
+++ src/server/data-dir.ts
@@ -4,6 +4,7 @@ import 'server-only'
 // NB: We cannot use logging in this file because the logger itself uses this
 // file to set itself up
 
+import { isDev } from '@/app/utility'
 import fs from 'fs'
 import path from 'path'
 
@@ -14,15 +15,19 @@ export function locateDataDir() {
   // NB: If we don't do this, we will end up writing to a read-only part
   // inside the Docker image
   const storagePath = process.env.DATA_DIR ?? path.join('.next')
-  const devStoragePath = path.join(__dirname, '..', '..', '.next')
+  const devStoragePath = path.join(__dirname, '..', '..')
 
   let sp = devStoragePath
   try {
-    fs.statSync(storagePath)
-    sp = storagePath
-    console.warn(`Using storage path ${sp}`)
+    if (!isDev) {
+      fs.statSync(storagePath)
+      sp = storagePath
+      console.warn(`Using data root path ${sp}`)
+    } else {
+      throw new Error('not dev')
+    }
   } catch (e) {
-    console.warn(`Using alternate storage path ${sp}`)
+    console.warn(`Using alternate data path ${sp}`)
   }
 
   return (dataDir = sp)
